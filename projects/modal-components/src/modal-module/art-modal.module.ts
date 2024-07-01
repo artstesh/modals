d5ebb814161da5283feb19f7f02a24d6eef68e2e
artstesh <artstesh@mail.ru>
2024-07-01 22:20:44 +0300

    1.0.1

--- projects/modal-components/src/modal-module/art-modal.module.ts
+++ projects/modal-components/src/modal-module/art-modal.module.ts
@@ -1,23 +1,14 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {DestructibleComponent} from './common/destructible.component';
-import {ModalComponent} from './components';
-import {LanguagePipe} from './common/language.pipe';
-import {ModalRootComponent} from './components/modal-root/modal-root.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DestructibleComponent } from './common/destructible.component';
+import { ModalComponent } from './components';
+import { LanguagePipe } from './common/language.pipe';
+import { ModalRootComponent } from './components/modal-root/modal-root.component';
 import { ModalBackdropComponent } from './components/modal/modal-backdrop/modal-backdrop.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [
-    DestructibleComponent,
-    LanguagePipe,
-    ModalComponent,
-    ModalRootComponent,
-    ModalBackdropComponent
-  ],
-  exports: [
-    ModalComponent, ModalRootComponent
-  ]
+  declarations: [DestructibleComponent, LanguagePipe, ModalComponent, ModalRootComponent, ModalBackdropComponent],
+  exports: [ModalComponent, ModalRootComponent],
 })
-export class ArtModalModule {
-}
+export class ArtModalModule {}
--- projects/modal-components/src/modal-module/components/modal/modal-backdrop/modal-backdrop.component.ts
+++ projects/modal-components/src/modal-module/components/modal/modal-backdrop/modal-backdrop.component.ts
@@ -1,14 +1,14 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input} from '@angular/core';
-import {ClassNameConstants} from "../../../models/class-name-constants.enum";
-import {ModalSettings} from "../../../models";
-import {CloseModalCommand} from "../../../messages";
-import {ModalPostboyService} from "../../../services/modal-postboy.service";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
+import { ClassNameConstants } from '../../../models/class-name-constants.enum';
+import { ModalSettings } from '../../../models';
+import { CloseModalCommand } from '../../../messages';
+import { ModalPostboyService } from '../../../services/modal-postboy.service';
 
 @Component({
   selector: 'art-modal-backdrop',
   templateUrl: './modal-backdrop.component.html',
   styleUrls: ['./modal-backdrop.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModalBackdropComponent {
   backdropClass: string = '';
@@ -19,13 +19,11 @@ export class ModalBackdropComponent {
   @Input() set settings(value: ModalSettings | undefined) {
     if (!value || this._settings.isSame(value)) return;
     this._settings = value;
-    this.backdropClass= this._settings.panelClass?.length ? `${this._settings.panelClass}-backdrop` : '';
+    this.backdropClass = this._settings.panelClass?.length ? `${this._settings.panelClass}-backdrop` : '';
     this.detector.detectChanges();
   }
 
-  constructor(private postboy: ModalPostboyService,
-              private detector: ChangeDetectorRef) {
-  }
+  constructor(private postboy: ModalPostboyService, private detector: ChangeDetectorRef) {}
 
   close(): void {
     this.postboy.fire(new CloseModalCommand(this._settings.id));
