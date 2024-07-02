06934c580c10fbd996e27cf33f64bc8a10108ced
artstesh <artstesh@mail.ru>
2024-07-02 10:41:29 +0300

    v1

--- projects/modal-components/src/modal-module/common/language.pipe.ts
+++ projects/modal-components/src/modal-module/common/language.pipe.ts
@@ -2,17 +2,12 @@ import { Injector, Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'artLanguagePipe',
-  pure: false,
+  pure: false
 })
 export class LanguagePipe implements PipeTransform {
   public constructor(private injector: Injector) {}
 
-  transform(value: any, pipeToken: any): any {
-    if (!pipeToken) {
-      return value;
-    } else {
-      let pipe = this.injector.get(pipeToken);
-      return pipe.transform(value);
-    }
+  transform(value: any, pipe:  PipeTransform | null): any {
+    return pipe?.transform(value)??value;
   }
 }
