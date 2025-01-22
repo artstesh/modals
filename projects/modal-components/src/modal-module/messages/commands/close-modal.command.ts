5e5c8cb6cdfe4422a844d52e0b326e4c47d5fb26
artstesh <artstesh@mail.ru>
2025-01-22 14:03:32 +0300

    v4

--- projects/modal-components/src/modal-module/messages/commands/close-modal.command.ts
+++ projects/modal-components/src/modal-module/messages/commands/close-modal.command.ts
@@ -6,11 +6,12 @@ export class CloseModalCommand extends PostboyGenericMessage {
     return CloseModalCommand.ID;
   }
   /**
-   * @class
-   * @param {string} modalId - The ID of the modal to be closed.
-   * @constructor
+   * Creates an instance of the class with the specified modal ID and a default result value.
+   *
+   * @param {string} modalId - The unique identifier for the modal.
+   * @param {boolean} [result=false] - The result status, defaulting to false.
    */
-  constructor(public modalId: string) {
+  constructor(public modalId: string, public result: boolean = false) {
     super();
   }
 }
