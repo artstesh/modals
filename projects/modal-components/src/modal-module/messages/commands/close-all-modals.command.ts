bddccc1d341e1873fc11d77d48058b26ee737ade
artstesh <artstesh@mail.ru>
2025-01-30 09:14:29 +0300

    wip

--- projects/modal-components/src/modal-module/messages/commands/close-all-modals.command.ts
+++ projects/modal-components/src/modal-module/messages/commands/close-all-modals.command.ts
@@ -1,4 +1,5 @@
 import { PostboyGenericMessage } from '@artstesh/postboy';
 
 export class CloseAllModalsCommand extends PostboyGenericMessage {
+  static ID: string = '828343e3-9dfc-4c6e-b7e4-ab9d5a13bd37';
 }
