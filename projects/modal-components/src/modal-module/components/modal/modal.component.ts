ab263dd376ca252536230e00f61a9fd599fbfec1
artstesh <artstesh@mail.ru>
2025-03-31 08:29:21 +0300

    1.0.7

--- projects/modal-components/src/modal-module/components/modal/modal.component.ts
+++ projects/modal-components/src/modal-module/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit, TemplateRef} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { DestructibleComponent } from '../../common/destructible.component';
 import { ModalPostboyService } from '../../services/modal-postboy.service';
 import { ModalSettings } from '../../models';
@@ -48,9 +48,7 @@ export class ModalComponent extends DestructibleComponent implements OnInit {
         .pipe(filter((cmd) => cmd.modalId === this._settings.id))
         .subscribe((cmd) => this.close(cmd.result)),
     );
-    this.subs.push(
-      this.postboy.sub(CloseAllModalsCommand).subscribe(() => this.close(false)),
-    );
+    this.subs.push(this.postboy.sub(CloseAllModalsCommand).subscribe(() => this.close(false)));
   }
 
   open(cmd: OpenModalCommand): void {
